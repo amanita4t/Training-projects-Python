a6be1b9a86241017b09b4f078288fc8afac12214
amanita4t <amanueltekalign4@gmail.com>
2025-01-15 18:42:54 +0300

    first test added

--- Django_projects/mysite/polls/views.py
+++ Django_projects/mysite/polls/views.py
@@ -3,20 +3,23 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.db.models import F
 from .models import Question, Choice
 from django.urls import reverse
-
+from django.views import generic
 # Create your views here.
-def index(request):
-    question = Question.objects.order_by("-pub_date")[:5]
-    return render(request, {"question": question})   
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+    def get_queryset(self):
+        return Question.objects.order_by("-pub_date")[:5]  
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/results.html", {"question" : question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html" 
 
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+    
 def vote(request, question_id):
     question = get_object_or_404(Question, pk= question_id)
     try:
