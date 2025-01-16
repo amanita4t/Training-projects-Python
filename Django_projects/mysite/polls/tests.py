7521a5ba6a9593bc671c87bc0f57f89a6ff22784
amanita4t <amanueltekalign4@gmail.com>
2025-01-16 19:54:31 +0300

    new test added

--- Django_projects/mysite/polls/tests.py
+++ Django_projects/mysite/polls/tests.py
@@ -1,6 +1,7 @@
 import datetime
 from django.utils import timezone
 from django.test import TestCase
+from django.urls import reverse
 
 from .models import Question
 
@@ -9,5 +10,20 @@ class QuestionModelTests(TestCase):
     def test_was_published_recently_with_future_question(self):
         time = timezone.now() + datetime.timedelta(days = 30)
         future_question = Question(pub_date = time)
-        self.assertIs(future_question.was_published_recently(), False)
+        return self.assertIs(future_question.was_published_recently(), False)
 
+    def test_was_published_recently_with_old_question(self):
+        time = timezone.now() - datetime.timedelta(days=2)
+        old_question = Question(pub_date = time)
+        return self.assertIs(old_question.was_published_recently(), False)
+
+def create_question(question_text, days):
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date = days)
+
+class QuestionIndexViewTests(TestCase):
+    def Test_no_question(self):
+        response = self.client(reverse("polls:index"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No polls are available.")
+        self.assertQuerySetEqual(response.context["latest_question_list"], [])
--- Django_projects/mysite/polls/views.py
+++ Django_projects/mysite/polls/views.py
@@ -4,13 +4,14 @@ from django.db.models import F
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 # Create your views here.
 
 class IndexView(generic.ListView):
     template_name = "polls/index.html"
     context_object_name = "latest_question_list"
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]  
+        return Question.objects.filter(pub_date__lte = timezone.now()).order_by("-pub_date")[:5]  
 
 class DetailView(generic.DetailView):
     model = Question
